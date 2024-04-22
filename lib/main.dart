2867105de99ed438ba3ae57eff75011577b80367
wahyukaprawi <wahyukaprawi123@gmail.com>
2024-04-22 23:42:06 +0800

    add code

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:dropdown_search/dropdown_search.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<String> country = ["Brazil", "Italia (Disabled)", "Tunisia", 'Canada'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Widget Button Dropdown"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: DropdownSearch<String>(
+          clearButtonProps: const ClearButtonProps(
+            isVisible: true,
+            icon: Icon(Icons.clear),
+          ),
+          popupProps: PopupProps.menu(
+            showSelectedItems: true,
+            disabledItemFn: (String s) => s.startsWith('I'),
+          ),
+          items: country,
+          dropdownDecoratorProps: const DropDownDecoratorProps(
+            dropdownSearchDecoration: InputDecoration(
+              labelText: "Menu mode",
+              hintText: "country in menu mode",
+            ),
+          ),
+          onChanged: print,
+          selectedItem: "Brazil",
+        ),
+      ),
+    );
+  }
+}
